6062ce2b67cec226407cc6f8f0184bf4b147a6ff
cavadev <claudio.vasquez.ar@gmail.com>
2019-07-05 13:25:02 +0100

    add django-cors-headers

--- backend/config/local.py
+++ backend/config/local.py
@@ -23,3 +23,7 @@ class Local(Common):
     EMAIL_HOST = 'localhost'
     EMAIL_PORT = 1025
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+    CORS_ORIGIN_WHITELIST = [
+        "http://localhost:3000"
+    ]
